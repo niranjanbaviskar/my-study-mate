f71c9aa750caba6301444038bee83b7d6fe0315a
niranjanbaviskar <157515954+niranjanbaviskar@users.noreply.github.com>
2025-03-08 15:18:05 +0530

    Update questions

--- src/pages/Questionaire.jsx
+++ src/pages/Questionaire.jsx
@@ -68,7 +68,7 @@ function Questionnaire() {
     console.log("Submitted answers:", answers)
     // Store answers 
     localStorage.setItem("questionnaireAnswers", JSON.stringify(answers))
-    // Redirect to ChatBot 
+    // Redirect to ChatBot page
     
     setTimeout(() => {
       navigate("/chatbot")
